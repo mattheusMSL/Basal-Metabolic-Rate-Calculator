d194a5abcaf0589e9294a6f278940eb49802305a
mattheusMSL <matthewpeterparker@gmail.com>
2023-01-01 03:00:30 -0300

    update

---
+++ Calculator/script.js
@@ -0,0 +1,14 @@
+let gender = document.getElementById("idg");
+let weight = document.getElementById("idw");
+let height = document.getElementById("idh");
+let age = document.getElementById("ida");
+
+function calculate(){
+
+
+    if(gender == "male"){
+
+    }else if(gender == "female"){
+
+    }
+}
