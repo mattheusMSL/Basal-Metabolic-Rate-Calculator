5d4beb9140b34b9abf46edd90be4c35800df07cf
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-02-20 17:30:06 -0300

    updates

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,28 @@
-let gender = document.getElementById("idg");
-let weight = document.getElementById("idw");
-let height = document.getElementById("idh");
-let age = document.getElementById("ida");
+function basalCaculator(gender,weight,height,age){
+    // const gender = document.getElementById("idg");
+    // const weight = document.getElementById("idw");
+    // const height = document.getElementById("idh");
+    // const age = document.getElementById("ida");
+    let calcular = 0;
 
-function calculate(){
+    if(gender == "male"|| gender == "Male"){
 
+       calcular = 66 + (13,8 * weight) + (5 * height) - (6,8 * age);
 
-    if(gender == "male"){
+    }else if(gender == "female" || gender == "Female"){
 
-    }else if(gender == "female"){
+       calcular =  655 + (9,6 * weight) + (1,8 * height) - (4,7 * age);
 
     }
+
+    return calcular.toFixed(2);
+
 }
+
+const gender = 'male';
+const weight = 80;
+const height = 175;
+const age = 20;
+
+const calcular = basalCaculator(gender,weight,height,age);
+console.log(calcular);
